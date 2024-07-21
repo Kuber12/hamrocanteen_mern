83c794a0c675e38d5c4f2704f757875a6649cc40
Rikin Tuladhar <110732947+RikinTuladhar@users.noreply.github.com>
2024-07-21 09:24:12 +0545

    login implemented

---
+++ frontend/src/apis/UserApi.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import axios from "axios"
+
+const UserApi = () => {
+    const URL = "http://localhost:3000";
+
+    async function loginUser(user) {
+      try{
+        const EndPoint = URL + "/api/user/login";
+        console.log(EndPoint)
+        const response = await axios.post(EndPoint,user);
+        const data = await response.data.message;
+        localStorage.setItem("token",JSON.stringify(data));
+        console.log(data)
+        return true; //if response is with no error
+      }catch(error){
+        console.log(error + " user login error")
+        return false;  //if response is with no error
+      }
+    }
+
+  
+    return {loginUser}
+}
+
+export default UserApi
--- frontend/src/pages/login/SignIn.jsx
+++ frontend/src/pages/login/SignIn.jsx
@@ -1,9 +1,32 @@
 import React, {  useState } from "react";
 import { CiUser } from "react-icons/ci";
 import { CiLock } from "react-icons/ci";
+import UserApi from "../../apis/UserApi"
+import { useNavigate} from 'react-router-dom'
 import "./SignIn.css";
 const SignIn = () => {
-  const [data,setData] = useState();
+const navigate = useNavigate();
+  const {loginUser} = UserApi();
+  const [user,setUser] = useState(
+    {
+      username:"",
+      password:""
+    }
+  );
+
+  // console.log(user)
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    loginUser(user).then((res)=>{
+      if(res == true){
+        alert("login success")
+        navigate("/")
+      }else{
+        alert("login failed")
+      }
+    }).catch(err=>console.log(err))
+  }
 
   return (
     <div className="w-full h-[100vh] backgroundCss  flex justify-center items-center">
@@ -12,15 +35,15 @@ const SignIn = () => {
           <img className=" object-contain w-[20rem] h-[10rem]" src="./Logo.png" alt="logo image" />
         </div>
         <div className="w-full h-[60%]">
-          <form action="" className="w-full h-[70%]">
+          <form onSubmit={handleSubmit} className="w-full h-[70%]">
             <div className="flex flex-col items-center justify-center w-full h-full gap-y-6">
             <div className="relative flex items-center justify-center w-full">
             <CiUser className="absolute left-10" size={18}/>
-            <input type="text" className="px-3 pl-10 py-1 w-[15rem] rounded-2xl" placeholder="Username"/>
+            <input onChange={(e)=>{setUser({...user,username:e.target.value})}} type="text" className="px-3 pl-10 py-1 w-[15rem] rounded-2xl" placeholder="Username"/>
             </div>
             <div className="relative flex items-center justify-center w-full">
             <CiLock    className="absolute left-10" size={18}/>
-            <input type="text" className="px-3 pl-10 py-1 w-[15rem] rounded-2xl" placeholder="Password"/>
+            <input onChange={(e)=>{setUser({...user,password:e.target.value})}} type="text" className="px-3 pl-10 py-1 w-[15rem] rounded-2xl" placeholder="Password"/>
             </div>
             <button className="px-8 py-1 font-bold text-white rounded-lg bg-[#393A3C]">Sign in</button>
             </div>
