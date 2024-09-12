f82edd3d2fadc3e6375a7746524ee5b5c35c978f
kuber12 <kubershakya123@hotmail.com>
2024-09-12 23:34:41 +0545

    sign in frontned

--- frontend/src/pages/login/SignIn.jsx
+++ frontend/src/pages/login/SignIn.jsx
@@ -30,22 +30,25 @@ const navigate = useNavigate();
 
   return (
     <div className="w-full h-[100vh] bg-tertiary  flex justify-center items-center">
-      <div className="w-[350px] space-y-3 backdrop-blur-sm   h-[450px] rounded-3xl  shadow-lg p-6 backdrop-filter bg-opacity-20 bg-white">
+      <div className="w-[350px] bg-secondary space-y-3  h-[450px] rounded-3xl  shadow-lg p-6">
+          <h1 className="text-center text-4xl font-bold">Sign <span className="text-primary">In</span></h1>
         <div className="w-full h-[40%]  flex justify-center items-center">
           <img className=" object-contain w-[20rem] h-[10rem]" src="./Logo.png" alt="logo image" />
         </div>
         <div className="w-full h-[60%]">
           <form onSubmit={handleSubmit} className="w-full h-[70%]">
+            
             <div className="flex flex-col items-center justify-center w-full h-full gap-y-6">
             <div className="relative flex items-center justify-center w-full">
-            <CiUser className="absolute left-10" size={18}/>
-            <input onChange={(e)=>{setUser({...user,username:e.target.value})}} type="text" className="px-3 pl-10 py-1 w-[15rem] rounded-2xl" placeholder="Username"/>
+              <CiUser className="absolute left-10 z-10" size={18}/>
+              <input onChange={(e)=>{setUser({...user,username:e.target.value})}} type="text" className="px-3 pl-10 py-1 w-[15rem] rounded-2xl drop-shadow-md" placeholder="Username"/>
             </div>
+
             <div className="relative flex items-center justify-center w-full">
-            <CiLock    className="absolute left-10" size={18}/>
-            <input onChange={(e)=>{setUser({...user,password:e.target.value})}} type="text" className="px-3 pl-10 py-1 w-[15rem] rounded-2xl" placeholder="Password"/>
+              <CiLock    className="absolute left-10 z-10" size={18}/>
+              <input onChange={(e)=>{setUser({...user,password:e.target.value})}} type="text" className="px-3 pl-10 py-1 w-[15rem] rounded-2xl drop-shadow-md" placeholder="Password"/>
             </div>
-            <button className="px-8 py-1 font-bold text-white rounded-lg bg-[#393A3C]">Sign in</button>
+            <button className="px-8 py-1 font-bold text-white rounded-lg bg-primary">Login</button>
             </div>
           </form>
         </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,8 +9,10 @@ export default {
       'primary': '#EC5856',
       'secondary': '#FFF0E9',
       'alternate': '#FDCC63',
+      'tertiary': '#F6CDB7',
       'lightTxt': '#B4ADA7',
-      'darkTxt': '#393A3C'
+      'darkTxt': '#393A3C',
+      'white': 'white'
     },
     extend: {},
   },
