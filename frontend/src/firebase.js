bd46a17d136732bc4b577befc3f0565d0f18d2ed
kuber12 <kubershakya123@hotmail.com>
2024-09-26 23:51:01 +0545

    firebase added

---
+++ frontend/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDUzzmTj4WXf1faJ0R2bXFrUy_6S2sZl8",
+  authDomain: "image-store-ca84a.firebaseapp.com",
+  projectId: "image-store-ca84a",
+  storageBucket: "image-store-ca84a.appspot.com",
+  messagingSenderId: "254433681567",
+  appId: "1:254433681567:web:90619541c4ed6b8ce5c2aa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
